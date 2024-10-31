8cd1df0cc0fad7b257c564b84f525789737db438
Sergey Frolov <sfrolov@gpmdata.ru>
2024-10-31 16:13:28 +0300

    refactor main method

--- database/services/database_services.py
+++ database/services/database_services.py
@@ -2,25 +2,34 @@ from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
 
 from sqlalchemy import select
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 
 from database.models.lesson import Lesson
 from database.models.subject import Subject
 
 
-async def get_all_future_lessons(
-        session: AsyncSession,
-        future_period_start: int,
-        future_period_end: int
-) -> list[Lesson]:
-    current_dttm_msc = datetime.now(ZoneInfo('Europe/Paris'))
+class DbService:
+    def __init__(self, dbDsn: str):
+        self.dbDsn = dbDsn
+        engine = create_async_engine(url=self.dbDsn)
+        self.session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
-    stmt = (
-        select(Lesson)
-        .where(Lesson.lesson_dttm >= current_dttm_msc + timedelta(hours=future_period_start))
-        .where(Lesson.lesson_dttm < current_dttm_msc + timedelta(hours=future_period_end))
-        .join(Subject)
-        .order_by(Lesson.created_at.desc())
-    )
-    result = await session.execute(stmt)
-    return result.scalars().all()
+    async def get_all_future_lessons(
+            self,
+            future_period_start: int,
+            future_period_end: int
+    ) -> list[Lesson]:
+        current_dttm_msc = datetime.now(ZoneInfo('Europe/Paris'))
+
+        stmt = (
+            select(Lesson)
+            .where(Lesson.lesson_dttm >= current_dttm_msc + timedelta(hours=future_period_start))
+            .where(Lesson.lesson_dttm < current_dttm_msc + timedelta(hours=future_period_end))
+            .join(Subject)
+            .order_by(Lesson.created_at.desc())
+        )
+
+        async with self.session_maker() as session:
+            result = await session.execute(stmt)
+            await session.close()
+            return result.scalars().all()
--- main.py
+++ main.py
@@ -2,13 +2,10 @@ import asyncio
 
 from aiogram import Bot
 from aiogram.client.default import DefaultBotProperties
-from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
-
 from configuration import Configuration
 from configuration.Configuration import load_config
-from database.services.database_services import get_all_future_lessons
+from database.services.database_services import DbService
 from database.services.sending_services import send_lessons_notification
-
 
 
 async def main():
@@ -17,10 +14,8 @@ async def main():
     bot = Bot(
         token=config.telegram.token, default=DefaultBotProperties(parse_mode="HTML")
     )
-    engine = create_async_engine(url=config.db.dsn)
-    session_maker = async_sessionmaker(engine, expire_on_commit=False)
-    async with session_maker() as session:
-        future_lessons = await get_all_future_lessons(session, 24, 25)
+    db_service = DbService(config.db.dsn)
+    future_lessons = await db_service.get_all_future_lessons(24, 25)
     await send_lessons_notification(bot, future_lessons, config.telegram.admin_login)
 
 
